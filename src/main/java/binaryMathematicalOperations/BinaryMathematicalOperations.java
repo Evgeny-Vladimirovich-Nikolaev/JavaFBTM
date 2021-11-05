cca92e099e53e5564db38a5c9bda3faab21d5901
Nobilis <nikolaev-evgeny@yandex.ru>
2021-11-05 22:57:23 +0300

    2021-11-05/22.52 ValueReceiver is completed

--- src/main/java/binaryMathematicalOperations/BinaryMathematicalOperations.java
+++ src/main/java/binaryMathematicalOperations/BinaryMathematicalOperations.java
@@ -3,7 +3,6 @@ public class BinaryMathematicalOperations {
     private int first, second;
 
     BinaryMathematicalOperations() {
-        System.out.println("BinaryMathematicalOperations was created");
         ValueReceiver valueReceiver = new ValueReceiver();
         first = valueReceiver.getFirstValue();
         second = valueReceiver.getSecondValue();
--- src/main/java/binaryMathematicalOperations/ValueReceiver.java
+++ src/main/java/binaryMathematicalOperations/ValueReceiver.java
@@ -1,63 +1,83 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
 public class ValueReceiver {
 
-    private int firstValue, secondValue;
-    private String firstEnter = "\nEnter the first number";
-    private String secondEnter = "\nEnter the second number";
-    private String condition = "Value must be between -2147483648 and 2147483647";
-    private String invalidNumber = "Invalid number\n";
-    private String invalidLine = " is not a number";
-    private String again = "Please, try again";
+    private int firstNumber, secondNumber;
+    private String firstMessage = "Enter two numbers between -2147483648 and 2147483647";
+    private String secondMessage = "Enter the second number";
+    private String condition = "The value must be between -2147483648 and 2147483647\n";
+    private String invalidNumber = " is invalid number\n";
+    private String invalidLine = " is not a number\n";
+    private String ioError = "Input output error";
+    private String error = "Something went wrong";
+    private String again = "Please try again";
+    private String firstValue = "The first number is ";
+    private String secondValue = "The second number is ";
 
     ValueReceiver(){
         setValues();
     }
 
     private void setValues(){
-        firstValue = receiveValue(firstEnter);
-        secondValue = receiveValue(secondEnter);
+        firstNumber = receiveValue(firstMessage, firstValue);
+        secondNumber = receiveValue(secondMessage, secondValue);
     }
 
     int getFirstValue() {
-        return firstValue;
+        return firstNumber;
     }
 
     int getSecondValue() {
-        return secondValue;
+        return secondNumber;
     }
 
-    private int receiveValue(String enter) {
-        int temp;
-        System.out.println(enter);
+    private int receiveValue(String message, String currentValue) {
+        int newNumber;
+        String temp = "";
+        System.out.println(message);
         while(true) {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             try {
-                try {
-                    temp = Integer.parseInt(br.readLine());
+                try {temp = br.readLine();
+                    newNumber = Integer.parseInt(temp);
                 } catch (NumberFormatException numEx) {
-                    System.out.println(numEx + invalidNumber);
+                    if(checkLine(temp)){
+                        System.out.println(temp + invalidNumber + condition + again);
+                    } else {
+                        System.out.println(temp + invalidLine + again);
+                    }
                     continue;
                 } catch (IOException numEx) {
-                    System.out.println(numEx + " Something went wrong");
+                    System.out.println(ioError);
                     continue;
                 }
                 break;
             } catch (Exception e) {
-                e.printStackTrace();
+                System.out.println(error);
             }
             try {
                 br.close();
             } catch (IOException e) {
-                e.printStackTrace();
+                System.out.println(ioError);
             }
         }
-        System.out.println("value is " + temp);
-        return temp;
+        System.out.println(currentValue + newNumber);
+        return newNumber;
+    }
+
+    private boolean checkLine(@NotNull String line){
+        char ch[] = line.toCharArray();
+        if(ch[0] != 45 && (ch[0] < 48 || ch[0] > 57)){
+            return false;
+        }
+        for(int i = 1; i < ch.length; i++){
+            if(ch[i] < 48 || ch[i] > 57)
+            return false;
+        }
+        return true;
     }
 }
