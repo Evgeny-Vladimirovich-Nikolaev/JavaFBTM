5949b942e80bbc412222c7789150bced13522b83
Nobilis <nikolaev-evgeny@yandex.ru>
2021-11-06 23:37:40 +0300

    2021-11-06/22.54  boolean resume() in BinaryMathematicalOperations with choice

--- src/main/java/binaryMathematicalOperations/BinaryMathematicalOperations.java
+++ src/main/java/binaryMathematicalOperations/BinaryMathematicalOperations.java
@@ -1,33 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BinaryMathematicalOperations {
 
     private int first, second;
     private String message = "Enter Y to continue or N to exit";
+    private String ioError = "Input output error";
+    private String error = "Something went wrong";
 
     BinaryMathematicalOperations() {
         initOperation();
     }
 
-    private void initOperation(){
-        do{
+    private void initOperation() {
+        do {
             initValues();
             calculate();
-        }while(resume());
+        } while (resume());
     }
 
-    private void initValues(){
+    private void initValues() {
         ValueReceiver valueReceiver = new ValueReceiver();
         first = valueReceiver.getFirstValue();
         second = valueReceiver.getSecondValue();
     }
 
-    private void calculate(){
-        System.out.println(new Calculator (first, second));
+    private void calculate() {
+        System.out.println(new Calculator(first, second));
     }
 
-    private boolean resume(){
-        boolean bool;
+    private boolean resume() {
+        boolean isReady;
+        String line = "";
+        InputStreamReader stream = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(stream);
+
         System.out.println(message);
-        return false;
+
+        while (true) {
+            try {
+                isReady = stream.ready();
+            } catch (IOException e) {
+                System.out.println(ioError);
+                continue;
+            }
+            if (!isReady) {
+                continue;
+            }
+            try {
+                line = br.readLine();
+            } catch (IOException e) {
+                System.out.println(ioError);
+                continue;
+            } catch (Exception exp) {
+                System.out.println(error);
+            }
+            if (line.equals("Y")){
+                return true;
+            }else if (line.equals("N")){
+                return false;
+            }else{
+                System.out.println(message);
+                continue;
+            }
+        }
     }
 }
 
