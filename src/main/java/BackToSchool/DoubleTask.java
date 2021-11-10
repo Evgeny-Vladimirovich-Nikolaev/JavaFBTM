01e7e5684a13daa7db9b7e86fb3cb15e544a74a9
Evgeny-Vladimirovich-Nikolaev <93443619+Evgeny-Vladimirovich-Nikolaev@users.noreply.github.com>
2021-11-10 12:52:12 +0300

    Update DoubleTask.java

--- src/main/java/BackToSchool/DoubleTask.java
+++ src/main/java/BackToSchool/DoubleTask.java
@@ -1,14 +1,9 @@
 public class DoubleTask {
 
-    DoubleTask() {
-        selectTask();
-    }
-
     private void selectTask() {
         int choise;
         while(true) {
-            ValueReceiver2 valueReceiver = new ValueReceiver2();
-            choise = valueReceiver.getValue();
+            choise = ValueReceiver2.receiveInt();
             if(choise < 0 || choise > 2) {
                 System.out.println("Выберите действие:");
                 continue;
@@ -18,5 +13,4 @@ public class DoubleTask {
         if(choise == 1) new QuadraticEquation();
         else if (choise == 2) new Factorial();
     }
-
 }
