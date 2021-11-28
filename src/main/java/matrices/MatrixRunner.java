6da21aac76b17f052d0128f97c8da4bb9b137ec1
Nobilis <nikolaev-evgeny@yandex.ru>
2021-11-28 12:22:32 +0300

    2021-11-28/12.20 Matrix
    findDeterminant находит детерминант матрицы 3х3.
    Попытки найти детерминант 2х2, 4х4, 5х5 методом findDeterminant безрезультатны.

--- src/main/java/matrices/MatrixRunner.java
+++ src/main/java/matrices/MatrixRunner.java
@@ -8,7 +8,13 @@ public class MatrixRunner {
         sm_4.matrixArray = new int[][]{{-4, -2, -7, 8}, {2, 7, 4, 9}, {2, 0, 6, -3}, {6, 4, -10, -4}};
         System.out.println(sm_4.findDeterminant());
         SquareMatrix sm_2 = new SquareMatrix(2);
-        sm_4.matrixArray = new int[][]{{-6, -3}, {-10, -4}};
-        System.out.println(sm_4.findDeterminant());
+        sm_2.matrixArray = new int[][]{{-6, -3}, {-10, -4}};
+        System.out.println(sm_2.findDeterminant());
+        SquareMatrix sm_3 = new SquareMatrix(3);
+        sm_3.matrixArray = new int[][]{{7, 4, 9}, {0, 6, -3}, {4, -10, -4}};
+        System.out.println(sm_3.findDeterminant());
+        SquareMatrix sm_5 = new SquareMatrix(5);
+        sm_5.matrixArray = new int[][]{{1, 2, 3, 4, 5}, {-1, 0, 2, 5, 3}, {1, 2, 2, 4, 6}, {-3, 1, -2, 4, 3}, {1, 2, 6, 7, -8}};
+        System.out.println(sm_5.findDeterminant());
     }
 }
