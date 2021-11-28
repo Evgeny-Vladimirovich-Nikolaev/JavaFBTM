effe6f3dc6c8a042d3742aa3b5ab7ef49f1020b1
Nobilis <nikolaev-evgeny@yandex.ru>
2021-11-28 23:54:17 +0300

    2021-11-28/23.52 Matrix
    protected int findDeterminant(int[][] arr)
    private int[][] getMinor(int column, int[][] ancestor)
    Неудачная попытка написать рекурсию.

--- src/main/java/matrices/MatrixRunner.java
+++ src/main/java/matrices/MatrixRunner.java
@@ -3,18 +3,19 @@ public class MatrixRunner {
     public static void main(String[] args) {
         SquareMatrix sm = new SquareMatrix(3);
         sm.matrixArray = new int[][]{{2, -1, 3}, {-4, 3, -2}, {6, 2, 1}};
-        System.out.println(sm.findDeterminant());
+        System.out.println(sm.findDeterminant_3x3());
         SquareMatrix sm_4 = new SquareMatrix(4);
         sm_4.matrixArray = new int[][]{{-4, -2, -7, 8}, {2, 7, 4, 9}, {2, 0, 6, -3}, {6, 4, -10, -4}};
-        System.out.println(sm_4.findDeterminant());
+        System.out.println(sm_4.findDeterminant_3x3());
         SquareMatrix sm_2 = new SquareMatrix(2);
         sm_2.matrixArray = new int[][]{{-6, -3}, {-10, -4}};
-        System.out.println(sm_2.findDeterminant());
+        System.out.println(sm_2.findDeterminant_3x3());
         SquareMatrix sm_3 = new SquareMatrix(3);
         sm_3.matrixArray = new int[][]{{7, 4, 9}, {0, 6, -3}, {4, -10, -4}};
-        System.out.println(sm_3.findDeterminant());
+        System.out.println(sm_3.findDeterminant_3x3());
         SquareMatrix sm_5 = new SquareMatrix(5);
         sm_5.matrixArray = new int[][]{{1, 2, 3, 4, 5}, {-1, 0, 2, 5, 3}, {1, 2, 2, 4, 6}, {-3, 1, -2, 4, 3}, {1, 2, 6, 7, -8}};
-        System.out.println(sm_5.findDeterminant());
+        System.out.println(sm_5.findDeterminant_3x3());
+        System.out.println(sm_5.findDeterminant(sm_5.matrixArray));
     }
 }
--- src/main/java/matrices/SquareMatrix.java
+++ src/main/java/matrices/SquareMatrix.java
@@ -6,7 +6,7 @@ public class SquareMatrix extends Matrix {
         super(lines, lines);
     }
 
-    int findDeterminant() {
+    int findDeterminant_3x3() {
         for(int column = 0; column < columns; column++) {
             int temp = 1;
             for (int i = 0, j = column; i < rows; i++) {
@@ -26,5 +26,37 @@ public class SquareMatrix extends Matrix {
             determinant -= temp;
         }
         return determinant;
+    }
+
+    protected int findDeterminant(int[][] arr) {
+        int dtr = 0;
+        if(arr.length == 1) {
+            return arr[0][0];
+        } else {
+            for(int j = 0; j < arr.length; j++) {
+                int multiplyer = 1;
+                if( j %2 == 1) {
+                    multiplyer = -1;
+                }
+                dtr += arr[0][j] * findDeterminant(getMinor(j, arr)) * multiplyer;
+            }
+        }
+        return dtr;
+    }
+
+    private int[][] getMinor(int column, int[][] ancestor) {
+        int[][] minor = new int[ancestor.length - 1][ancestor.length - 1];
+        for(int j = 0; j < ancestor.length; j++) {
+            for(int vector = 0; vector < ancestor.length; vector++) {
+                if(vector == j) {
+                    continue;
+                } else {
+                    for(int i = 0; i < minor.length; i++) {
+                        minor[i][vector] = ancestor[i + 1][j];
+                    }
+                }
+            }
+        }
+        return minor;
     }
 }
