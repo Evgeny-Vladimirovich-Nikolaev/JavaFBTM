21501dc6b68f7e2eeaa0f44b7cce903c7d06cb84
Nobilis <nikolaev-evgeny@yandex.ru>
2021-11-30 08:06:40 +0300

    2021-11-29/23.42 MatrixPresentation
    Первая редакция форматированного вывода матрицы в консоль.

--- src/main/java/matrices/SquareMatrix.java
+++ src/main/java/matrices/SquareMatrix.java
@@ -24,3 +24,26 @@ public class SquareMatrix extends Matrix {
         return dtr;
     }
 }
+
+class MinorGetter {
+
+    private int[][] sourceArray;
+    private int[][] minor;
+    private int columnElem;
+
+    MinorGetter(int[][] sourceArray, int columnElem) {
+        this.sourceArray = sourceArray;
+        this.columnElem = columnElem;
+        minor = new int[sourceArray.length - 1][sourceArray.length - 1];
+    }
+
+    int[][] getMinor() {
+        for (int srcRow = 0, destRow = 0; srcRow < sourceArray.length; srcRow++) {
+            if (srcRow != columnElem) {
+                System.arraycopy(sourceArray[srcRow], 1, minor[destRow++], 0, minor.length);
+            }
+        }
+        return minor;
+    }
+}
+
