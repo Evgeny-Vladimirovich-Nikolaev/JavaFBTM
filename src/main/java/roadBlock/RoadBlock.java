6a1e0164638233b737a4200500df48e298859aac
Nobilis <nikolaev-evgeny@yandex.ru>
2021-12-09 23:33:06 +0300

    2021-12-09/23.26 interface Crossable
    Интерфейс Crossable объявляет метод boolean roadBlockCross(int speed), который нужно имплементировать в классы NonStandardCar и NonStandardTruck. В RoadBlock инициализируются статические поля со значениями, ограничивающими проезд. В класс можно передать ArrayList машин.

--- src/main/java/roadBlock/RoadBlock.java
+++ src/main/java/roadBlock/RoadBlock.java
@@ -1,2 +1,16 @@
 public class RoadBlock {
+
+    private static int speedLimit = 80;
+    private static int extraSpeedLimit = 100;
+    private static double weightLimit = 8.0;
+    private static double heightLimit = 4.0;
+    private static double widthLimit = 2.5;
+
+   // private ArrayList<Automobile> automobiles;
+
+    RoadBlock() {}
+
+    /*RoadBlock(ArrayList<Automobile> automobiles) {
+        this.automobiles = automobiles;
+    }*/
 }
--- src/main/java/roadBlock/nonStandardAuto/NonStandardCar.java
+++ src/main/java/roadBlock/nonStandardAuto/NonStandardCar.java
@@ -1,5 +1,10 @@
-public class NonStandardCar extends Automobile{
+public class NonStandardCar extends Automobile implements Crossable{
     public NonStandardCar(String licensePlate) {
         super(new LicenseGenerator().getNewLicense());
+    }
+
+    @Override
+    public boolean roadBlockCross(int speed) {
+        return true;
     }
 }
