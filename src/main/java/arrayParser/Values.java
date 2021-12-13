53d25079d0da41e7aaf1d47836cb64a564be2b2a
Nobilis <nikolaev-evgeny@yandex.ru>
2021-12-13 18:56:48 +0300

    2021-12-13/18.56 StringArrayParser
    Начало выполнения задания.

---
+++ src/main/java/arrayParser/Values.java
@@ -0,0 +1,29 @@
+public enum Values {
+    ZERO("0"),
+    ONE("1"),
+    TWO("2"),
+    TREE("3"),
+    FOUR("4"),
+    FIVE("five"),
+    SIX("6"),
+    SEVEN("7"),
+    EIGHT("8"),
+    NINE("9"),
+    TEN("10"),
+    ELEVEN("11"),
+    TWELVE("12"),
+    THIRTEEN("13"),
+    FOURTEEN("14"),
+    FIFTEEN("15"),
+    SIXTEEN("16"),
+    SEVENTEEN("17"),
+    EIGHTEEN("18"),
+    NINETEEN("19"),
+    TWENTY("20");
+
+    private String val;
+
+    Values(String val) {
+        this.val = val;
+    }
+}
