acf854a6a611d295215af52137fedc6a0296fb2b
Nobilis <nikolaev-evgeny@yandex.ru>
2021-12-16 13:21:49 +0300

    2021-12-16/13.08 ArrayParse
    Проверка массива в классе StringArrayParser перенесена в конструктор. В классе ArrayParseRunner упрощена логика в методе main.

--- src/main/java/roadBlock/RoadBlock.java
+++ src/main/java/roadBlock/RoadBlock.java
@@ -3,6 +3,12 @@ import java.util.List;
 
 public class RoadBlock {
 
+    private static int speedLimit = 80;
+    private static int extraSpeedLimit = 100;
+    private static double weightLimit = 8.0;
+    private static double heightLimit = 4.0;
+    private static double widthLimit = 2.5;
+
     private List<Crossable> crossables;
 
     RoadBlock(ArrayList<Crossable> crossables) {
