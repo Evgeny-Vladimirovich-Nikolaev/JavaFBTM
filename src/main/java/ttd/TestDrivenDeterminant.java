e7ad5d9142d408a73e91fc8538feefd09178536b
Nobilis <nikolaev-evgeny@yandex.ru>
2022-01-27 20:50:42 +0300

    2021-01-27/19.58 TestDrivenDeterminant
    реализован метод findByCycle(int[][]) с успешным прохождением Junit-теста.

--- src/main/java/ttd/TestDrivenDeterminant.java
+++ src/main/java/ttd/TestDrivenDeterminant.java
@@ -7,24 +7,36 @@
 public class TestDrivenDeterminant {
 
     public int findBySeparateActions(int[][] matrix) {
-        int determinant;
         if(matrix.length != 3 || matrix[0].length != 3) {
             throw new IllegalArgumentException();
         }
-        determinant =
-                matrix[0][0] * matrix[1][1] * matrix[2][2]
+        return  matrix[0][0] * matrix[1][1] * matrix[2][2]
                 + matrix[0][1] * matrix[1][2] * matrix[2][0]
                 + matrix[0][2] * matrix[1][0] * matrix[2][1]
                 - matrix[0][2] * matrix[1][1] * matrix[2][0]
                 - matrix[0][1] * matrix[1][0] * matrix[2][2]
                 - matrix[0][0] * matrix[1][2] * matrix[2][1];
-        return determinant;
     }
 
     public int findByCycle (int[][] matrix) {
         int determinant = 0;
-        if(matrix.length != 3 || matrix[0].length != 3) {
-            throw new IllegalArgumentException();
+        for(int column = 0; column < matrix.length; column++) {
+            int temp = 1;
+            for (int i = 0, j = column; i < matrix.length; i++) {
+                temp *= matrix[i][j];
+                j++;
+                j %= matrix.length;
+            }
+            determinant += temp;
+        }
+        for(int column = 0; column < matrix.length; column ++) {
+            int temp = 1;
+            for(int i = matrix.length - 1, j = column; i >= 0; i--) {
+                temp *= matrix[i][j];
+                j++;
+                j %= matrix.length;
+            }
+            determinant -= temp;
         }
         return determinant;
     }
